1cbad81363fa9015b379feb49cc8b47d1ce4bb65
Yusuf Oyinlola <yuzstackinnovation@gmail.com>
2026-01-11 13:22:20 +0100

    Make the input elements in the 'FormSplitBill' controlled

--- src/components/FormSplitBill.jsx
+++ src/components/FormSplitBill.jsx
@@ -1,21 +1,55 @@
+import { useState } from 'react';
 import Button from './Button';
 
 function FormSplitBill({ selectedFriend }) {
+  const [billValue, setBillValue] = useState('');
+  const [userExpense, setUserExpense] = useState('');
+  const [billPayer, setBillPayer] = useState('user');
+
+  const friendExpense = billValue ? billValue - userExpense : '';
+
+  const handleSubmit = function (e) {
+    e.preventDefault();
+
+    if (!billValue) return;
+  };
+
   return (
-    <form className='form-split-bill'>
+    <form onSubmit={handleSubmit} className='form-split-bill'>
       <h2>Split Bill with {selectedFriend.name}</h2>
 
       <label htmlFor='bill-value'>💰 Bill value</label>
-      <input type='number' id='bill-value' autoFocus />
+      <input
+        type='number'
+        id='bill-value'
+        autoFocus
+        value={billValue}
+        onChange={e => setBillValue(Number(e.target.value))}
+      />
 
       <label htmlFor='user-expense'>🧍 Your expense</label>
-      <input type='number' id='user-expense' />
+      <input
+        type='number'
+        id='user-expense'
+        value={userExpense}
+        onChange={e =>
+          setUserExpense(
+            Number(e.target.value) > billValue
+              ? userExpense
+              : Number(e.target.value)
+          )
+        }
+      />
 
       <label htmlFor='friend-expense'>👬 {selectedFriend.name}'s expense</label>
-      <input type='number' id='friend-expense' disabled />
+      <input type='number' id='friend-expense' disabled value={friendExpense} />
 
       <label htmlFor='bill-payer'>🤑 Who is paying the bill?</label>
-      <select id='bill-payer'>
+      <select
+        id='bill-payer'
+        value={billPayer}
+        onChange={e => setBillPayer(e.target.value)}
+      >
         <option value='user'>You</option>
         <option value='friend'>{selectedFriend.name}</option>
       </select>
